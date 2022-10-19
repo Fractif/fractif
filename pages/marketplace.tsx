6896534e050813ea1a402a41ef082c5dad29d9f2
azerpas <xskylix@hotmail.fr>
2022-10-19 12:02:49 +0200

    feat: add module paths resolution

---
+++ pages/marketplace.tsx
@@ -0,0 +1,11 @@
+import Layout from '@components/Layout'
+
+const Marketplace = () => {
+    return (
+        <Layout title="Home | Next.js + TypeScript Example">
+            <h1>Marketplace</h1>
+        </Layout>
+    )
+}
+
+export default Marketplace
