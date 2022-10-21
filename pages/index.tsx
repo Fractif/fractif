34ff205e4571b7b8321a7b489706ec43457df581
Tixwell <teoc.cricelli@gmail.com>
2022-10-21 10:32:26 +0200

    feat: integrate wallet modal to main page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,12 @@
 import Link from 'next/link'
 import Layout from '@components/Layout'
+import SelectWalletModal from '@components/wallet/modalConnect'
+
 
 const IndexPage = () => (
   <Layout title="Home | Next.js + TypeScript Example">
     <h1>Hello Next.js 👋</h1>
+    <SelectWalletModal />
     <p>
       <Link href="/about">
         <a>About</a>
