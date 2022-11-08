c25b75bb91fbfb59e6f64ee5462f7cc57528a681
Tixwell <teoc.cricelli@gmail.com>
2022-11-08 11:17:30 +0100

    fix: delete app directory setup

---
+++ pages/index.tsx
@@ -0,0 +1,32 @@
+import { BackgroundImage, createStyles } from '@mantine/core';
+//Import landing page components
+import Hero from '@components/landingpage/hero'
+import Features from '@components/landingpage/features'
+
+const useStyles = createStyles((theme) => ({
+    background_effect: {
+        position: 'absolute', 
+        top: 0, 
+        right: 0, 
+        zIndex: -1, 
+        width: "30%", 
+        height: '100%'
+    },
+}));
+
+
+export default function LandingPage() {
+    const { classes } = useStyles();
+
+    return (
+        <>
+            <BackgroundImage
+                src="/landing_effect.svg"
+                className={classes.background_effect}
+            />
+            <Hero />
+            <Features />
+
+        </>
+    )
+}
