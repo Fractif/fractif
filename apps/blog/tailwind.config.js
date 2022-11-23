3da2b280f052c1ce2c5ef54dce63b59b6c6dd371
azerpas <xskylix@hotmail.fr>
2022-11-23 13:11:12 +0100

    fix: blog - tailwind config

--- apps/blog/tailwind.config.js
+++ apps/blog/tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./components/**/*.tsx', './pages/**/*.tsx'],
+  content: ['./src/**/*.{js,ts,jsx,tsx}'],
   theme: {
     extend: {
       colors: {
