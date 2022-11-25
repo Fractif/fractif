a3b441a82b3feb23c4fed5a7a1f590662554a4f8
Tixwell <teoc.cricelli@gmail.com>
2022-11-25 11:08:35 +0100

    feat: add link to detailled roadmap

--- apps/landing/src/constants/index.ts
+++ apps/landing/src/constants/index.ts
@@ -1,6 +1,7 @@
 export const TERM_AND_SERVICES_URL = '/terms';
 export const DISCLAIMER_URL = '/disclaimer';
 export const PRIVACY_POLICY_URL = '/privacy-policy';
+export const ROADMAP_URL = '/roadmap';
 export const DISCORD_URL = '/discord';
 export const TWITTER_URL = '/twitter';
 export const OPENSEA_URL = '/openSea';
