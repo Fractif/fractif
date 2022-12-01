fe08a553d016beca1e8b620286ed45db48859a3d
Azerpas <xskylix@hotmail.fr>
2022-12-01 13:31:13 +0000

    feat: added fractif-ui

--- apps/blog/src/pages/_document.tsx
+++ apps/blog/src/pages/_document.tsx
@@ -1,13 +1,23 @@
 import { Html, Head, Main, NextScript } from 'next/document'
+import { FractifUiTheme } from 'fractif-ui'
+import { MantineProvider } from '@mantine/core'
 
 export default function Document() {
-  return (
-    <Html lang="en">
-      <Head />
-      <body>
-        <Main />
-        <NextScript />
-      </body>
-    </Html>
-  )
+    return (
+        <Html lang="en">
+            <Head />
+            <MantineProvider
+                // theme import from fractif-ui causes error in _document.tsx, check:
+                // @link https://github.com/Fractif/fractif/issues/55
+                // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+                // @ts-ignore
+                theme={FractifUiTheme}
+            >
+                <body>
+                    <Main />
+                    <NextScript />
+                </body>
+            </MantineProvider>
+        </Html>
+    )
 }
